bed76ac192233c092b2e8e85d851fd0200f7fca7
= <=>
2021-11-05 01:32:42 -0400

    All done by the grace of almighty

--- src/components/MyOrder/MyOrder.js
+++ src/components/MyOrder/MyOrder.js
@@ -6,52 +6,51 @@ import MenuBar from '../MenuBar/MenuBar'
 
 export default function MyOrder() {
 
-    const { user,isLoading} = FirebaseConnect();
+    const { user, isLoading } = FirebaseConnect();
     const [myorder, setMyOrder] = useState([]);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [tripCancel,setTripCancel] = useState(false);
+    const [tripCancel, setTripCancel] = useState(false);
+    const [dataLoaded,setDataLoaded] = useState(false);
 
-
-    const DeletePlan = (tid) =>{
+    const DeletePlan = (tid) => {
+        setTripCancel(false);
         const ans = window.confirm("Are you sure want to cancel this trip?");
-        if(ans){
-            setTripCancel(false);
-            fetch(`https://mighty-river-13958.herokuapp.com/delete/${tid}`,{
-                method:'DELETE'
+        if (ans) {
+            setTripCancel(true);
+            fetch(`https://mighty-river-13958.herokuapp.com/delete/${tid}`, {
+                method: 'DELETE'
             })
-            .then(res=>res.json())
-            .then(data=>console.log(data));
-            const newList = myorder.filter(data=>data.tid!==tid);
+                .then(res => res.json())
+                .then(data => console.log(data));
+            const newList = myorder.filter(data => data.tid !== tid);
             setMyOrder(newList);
-            
         }
-        
+
     }
 
     useEffect(() => {
-        setIsLoaded(false);
         fetch(`https://mighty-river-13958.herokuapp.com/order/${user.uid}`)
             .then(res => res.json())
-            .then(data => {setMyOrder(data);setIsLoaded(true);})
-        
-    }, [user,tripCancel])
+            .then(data => setMyOrder(data))
+
+    }, [user, tripCancel])
 
     return (
         <div>
             <MenuBar></MenuBar>
-                {
-                    isLoaded || isLoading ?
-                        <div className='row'>
-                           { myorder.map(data=> 
-                                <div key={data._id} className='col-md-4 border border-3 border-success m-2 p-2'>
-                                    <h1>Package: {data.PackageName}</h1>
-                                    <h3>Status: {data.status}</h3>
-                                    <button onClick={()=>DeletePlan(data.tid)} className='btn btn-danger'>Cancel</button>
-                                </div>
-                            )}
-                        </div>
-                        : <div className='d-flex justify-content-center'><Spinner animation='border'/></div>
-                }
+            {
+                (isLoading  || myorder.length===0) ?
+                    <div className='d-flex justify-content-center'><Spinner animation='border' /></div> :
+
+                    <div className='row'>
+                        {myorder.map(data =>
+                            <div key={data._id} className='col-md-4 border border-3 border-success m-2 p-2'>
+                                <h1>Package: {data.PackageName}</h1>
+                                <h3>Status: {data.status}</h3>
+                                <button onClick={() => DeletePlan(data.tid)} className='btn btn-danger'>Cancel</button>
+                            </div>
+                        )}
+                    </div>
+            }
             <Footer></Footer>
         </div>
     )
